7660bdebf034cfca9ed0079a6e0d2411ce10e961
Unknown <ingermanm@hotmail.com>
2018-05-28 17:48:27 -0600

    Tarea  Clase 8

--- CocaoPODS/CONTROLLER/ViewController.swift
+++ CocaoPODS/CONTROLLER/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import SVProgressHUD
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        SVProgressHUD.show()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ CocaoPODS/MODEL/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  CocaoPODS
+//
+//  Created by Inger Montenegro on 5/26/18.
+//  Copyright © 2018 Inger Montenegro. All rights reserved.
+//
+
+import Foundation
